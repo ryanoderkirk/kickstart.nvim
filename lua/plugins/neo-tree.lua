ef90d5de97f6e60bfbf0773108f338d1c8da56d8
Ryan O <ryan.oderkirk@gmail.com>
2025-10-30 06:14:10 -0400

    add plugins folder. remove more unnecessary comments

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons', -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  },
+}
